a797385f3cfc89514838a86ebaea790f8618314f
vire <code.vire@gmail.com>
2017-03-14 22:58:31 +0100

    failing header test

--- src/Recipe.spec.js
+++ src/Recipe.spec.js
@@ -13,19 +13,20 @@ const Recipe = ({ title, created }) => {
 
 // Step 1 - create the test bellow
 describe('Recipe', () => {
-  it('should render a title into h2', () => {
-    const recipe = { title: 'Tasty foo bar!' };
-    const wrapper = shallow(<Recipe {...recipe}/>);
-    const expected = recipe.title;
-    const actual = wrapper.find('h2').text();
-    expect(actual).toEqual(expected);
-  });
-
-  it('should render the create date into a span', () => {
+  describe('header', () => {
     const recipe = { title: 'Tasty foo bar!', created: 'March 14 2017' };
-    const wrapper = shallow(<Recipe {...recipe}/>);
-    const expected = recipe.created;
-    const actual = wrapper.find('span').text();
-    expect(actual).toEqual(actual);
+    const getHeader = () => shallow(<Recipe {...recipe} />).find('.header');
+
+    it('should render a title into h2', () => {
+      const expected = recipe.title;
+      const actual = getHeader().find('h2').text();
+      expect(actual).toEqual(expected);
+    });
+
+    it('should render the create date into a span', () => {
+      const expected = recipe.created;
+      const actual = getHeader().find('span').text();
+      expect(actual).toEqual(expected);
+    });
   });
 });
