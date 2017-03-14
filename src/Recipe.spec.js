9a0b28f2f74e5444a56dfc6df6b6738d18844a71
vire <code.vire@gmail.com>
2017-03-15 00:21:44 +0100

    failing ingredients test

--- src/Recipe.spec.js
+++ src/Recipe.spec.js
@@ -15,10 +15,18 @@ const Recipe = ({ title, created }) => {
 
 // Step 1 - create the test bellow
 describe('Recipe', () => {
-  describe('header', () => {
-    const recipe = { title: 'Tasty foo bar!', created: 'March 14 2017' };
-    const getHeader = () => shallow(<Recipe {...recipe} />).find('.header');
+  const recipe = {
+    title: 'Tasty foo bar!',
+    created: 'March 14 2017',
+    ingredients: [
+      '100g chocolate',
+      '200g flour',
+      '2 eggs'
+    ]
+  };
 
+  describe('header', () => {
+    const getHeader = () => shallow(<Recipe {...recipe} />).find('.header');
     it('should render a title into h2', () => {
       const expected = recipe.title;
       const actual = getHeader().find('h2').text();
@@ -30,5 +38,16 @@ describe('Recipe', () => {
       const actual = getHeader().find('span').text();
       expect(actual).toEqual(expected);
     });
+  });
+
+  describe('main-section', () => {
+    it('should contain `left` column with list of ingredients', () => {
+      const wrapper = shallow(<Recipe {...recipe} />).find('.left-column li');
+      const actual = wrapper.map(node => node.text());
+      const expected = recipe.ingredients;
+      expect(actual).toEqual(expected);
+    });
+
+    it('should contain `right` column for instructions');
   });
 });
