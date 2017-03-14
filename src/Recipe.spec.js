01125dbd381a5350c055225944719994060a2424
vire <code.vire@gmail.com>
2017-03-14 22:39:16 +0100

    failing recipe.title test

--- src/Recipe.spec.js
+++ src/Recipe.spec.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-// Step 2 - when your test complains, that `<Recipe />` does not exist create a minimal impl
 const Recipe = () => {
   return (
     <div>Holla!</div>
   );
 };
 
-// Step 1 - create the test bellow
 describe('Recipe', () => {
-  it('should render "Holla!', () => {
-    // create an enzyme wrapper
-    const wrapper = shallow(<Recipe />); // see http://airbnb.io/enzyme/docs/api/shallow.html
-    // get the text value
-    const actual = wrapper.text(); // see http://airbnb.io/enzyme/docs/api/ShallowWrapper/text.html
-    const expected = 'Holla!';
+  it('should render a title into h2', () => {
+    const recipe = { title: 'Tasty foo bar!' };
+    const wrapper = shallow(<Recipe {...recipe}/>);
+    const expected = recipe.title;
+    const actual = wrapper.find('h2').text();
     expect(actual).toEqual(expected);
   });
 });
